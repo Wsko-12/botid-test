378b6fdc2ebe9d8a804b9e9b130ed5391cdfdb7a
Uladzislau Vasko <vaskovladislav@gmail.com>
2025-08-20 15:51:45 +0300

    botid init

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,8 @@
 import {NextRequest, NextResponse} from 'next/server'
+import {checkBotId} from "botid/server";
 
 export async function POST(request: NextRequest) {
-    return NextResponse.json({message: 'Action'})
+    const verification = await checkBotId();
+
+    return NextResponse.json(verification)
 }
